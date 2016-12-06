9b8c66a28b540f3aa7f9aa7816f179b84fac6d6a
inhwa.ma <inhwa.ma@lge.com>
2016-12-06 17:55:08 +0900

    [CONFIGURATION][DEVELSETTING] Delete Critical personal inform.
    Delete Personal informations.
    
    lol

--- bin/mount_cloud.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo mount.cifs -o username='inhwa.ma',password='inhwa.ma',uid='inhwama',rw //10.168.176.121/inhwa.ma ~/cloud/android/
-sudo mount.cifs -o username='inhwa.ma',password='inhwa.ma',uid='inhwama',rw //10.168.178.139/inhwa.ma ~/cloud/lune/
-sudo mount.cifs -o username='inhwa.ma',password='akfkdhdl00!',uid='inhwama',rw //10.168.142.163/inhwa.ma ~/cloud/cluster/
-sudo mount.cifs -o username='j1',password='j1',uid='inhwama',rw '//10.186.123.56/j1' ~/cloud/j1
-
--- bin/mount_cluster.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo mount.cifs -o username='inhwa.ma',password='akfkdhdl00!',uid='inhwama',rw '//10.186.123.233/Cluster/06.Porsche J1' ~/Cluster/J1
-
